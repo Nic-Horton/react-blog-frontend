eda6ce6588095f9de5f6dd0639efb81f0fdc269b
Nic-Horton <nic.horton00@gmail.com>
2023-10-09 18:25:58 -0500

    Added style to page that renders blog posts using MUI

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,31 +1,58 @@
 import React,{useEffect, useState}from 'react'
 import {Link } from 'react-router-dom'
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
 
 function Blogs() {
   const [blogs,setBlogs] = useState([]);
-  
-  const fetchBlogs = () => {
-    return fetch("http://localhost:3001/blogs").then((res)=>res.json()).then((d)=>setBlogs(d))
-  }
 
   useEffect(() => {
-    fetchBlogs();
+    fetch("http://localhost:3001/blogs")
+    .then((res)=>res.json())
+    .then((d)=>setBlogs(d))
   }, [])
 
+  const CardJSX = () =>{
+   return blogs.map((blog) =>{
+      return (
+        <Grid item xs={12} md={6} key={blog.id}>
+          <CardActionArea component={Link} to={`/blogs/${blog.id}`} >
+            <Card sx={{ display: 'flex', bgcolor: 'grey.A400'}}>
+              <CardContent sx={{ flex: 1 }}>
+                <Typography component="h2" variant="h5">
+                  {blog.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </CardActionArea>
+        </Grid>
+      )
+    })
+  }
+
   return (
-    <>
-    <h1>Blog List
-      <br />
-    </h1>
-
-    <ul>
-    {blogs.map((blog) => {
-      return (<li key={blog.id}><Link to={`/blogs/${blog.id}`}>{blog.title}</Link></li>)
-    })}
-    </ul>
-
-    </>
-  )
+    <Container maxWidth="lg">
+      <Typography
+          component="h2"
+          variant="h2"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+          mt={3}
+          mb={3}
+        >
+          Blogs
+      </Typography>
+      <Grid container spacing={4}>
+        <CardJSX/>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Blogs
