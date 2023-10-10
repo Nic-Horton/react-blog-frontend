85fd019c5bee0a25cbcbbaef9c40016ec5d115a2
Nic-Horton <nic.horton00@gmail.com>
2023-10-10 17:28:38 -0500

    Added style to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,33 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 function HomePage({isLoggedIn, user}) {
   return (
     <div>
-			<h1>Home Page</h1>
-			<h3>{isLoggedIn ? (`Hello ${user}`)  : 'Welcome!'}</h3>	
+      <Typography
+          component="h2"
+          variant="h2"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+          mt={3}
+          mb={3}
+        >
+          Home
+      </Typography>
+      <Typography
+          component="h5"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+          mt={3}
+          mb={3}
+        >
+          {isLoggedIn ? (`Welcome back, ${user}!`)  : 'Welcome!'}
+      </Typography>
 		</div>
   )
 }
